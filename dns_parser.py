ceb897378cb1be19513f04083b53f2dd81fa4476
vladimir <vladimirhoris@gmail.com>
2022-09-20 17:08:22 +0700

    доделал и расписал мини-доку

--- dns_parser.py
+++ dns_parser.py
@@ -1,71 +1,129 @@
-import logging
-import pickle
 import re
-import time
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 
-import configs_shops.config_dns
+import requests
+from bs4 import BeautifulSoup
 
-OPTIONS = webdriver.ChromeOptions()
-OPTIONS.add_argument(configs_shops.config_dns.ua)
-OPTIONS.add_argument(configs_shops.config_dns.webdriver_disable)
-OPTIONS.headless = True
-SERVICE = Service(executable_path='/home/vladimir/Pythonproject/parser-vc/configs_shops/chromedriver')
-DRIVER = webdriver.Chrome(service=SERVICE, options=OPTIONS)
+from configs_shops import config_dns
 
 
-def get_data_dns() -> dict:
-    '''
-    :param pages:
-    :return dict :
+def scraper_dns():
+    """
     Функция парсит сайт dns-shop.ru на поиск доступных видеокарт по определенным фильтрам
-    '''
+    """
 
-    product_result_dns = {}
-    product_num = 0
-    pages = get_pages()
-    for page in range(1, pages + 1):
-        try:
-            DRIVER.get(f'https://www.dns-shop.ru/catalog/17a89aab16404e77/videokarty/?p={page}')
-            time.sleep(2)
-            prods = DRIVER.find_elements(By.CLASS_NAME, 'catalog-product')
-            for prod in prods:
-                if 'RTX' in prod.find_element(By.CLASS_NAME, 'catalog-product__name').text and \
-                        'В наличии' in prod.find_element(By.CLASS_NAME, 'catalog-product__avails').text.split('\n')[0]:
-                    product_num += 1
-                    product_result_dns[product_num] = {
-                        'product_name': prod.find_element(By.CLASS_NAME, 'catalog-product__name').text,
-                        'product_link': prod.find_element(By.CLASS_NAME, 'catalog-product__name').get_attribute('href'),
-                        'product_price': prod.find_element(By.CLASS_NAME, 'catalog-product__buy').text.split('\n')[0],
-                        'product_available': 1
-                    }
-        except Exception as ex:
-            logging.exception(msg=ex)
-    DRIVER.close()
-    DRIVER.quit()
+    # Создаем сессию
+    with requests.Session() as session:
+        product_result_dns = {}
+
+        # Получаем кол-во страниц пагинации через атрибут href и ищем число при помощи регулярки
+        response_pagination = session.get(config_dns.url_dns, cookies=config_dns.cookies_dns,
+                                          headers=config_dns.headers_dns)
+        soup_pagination = BeautifulSoup(response_pagination.text, 'lxml')
+        elem_pagination = soup_pagination.find_all('a',
+                                                   class_='pagination-widget__page-link pagination-widget__page-link_last')
+        # Регулярка
+        pages_count = int(re.findall(r'p=(\d*)', str(elem_pagination[0].get('href')))[0])
+
+        # Создаем списки с именами, ценами и доступности
+        products_name = []
+        products_price = []
+        products_available = []
+
+        # Проходим по полученным страницам
+        for page in range(1, pages_count + 1):
+
+            # Делаем запрос к текущей странице page
+            response = session.get(f'{config_dns.url_dns}&p={page}', cookies=config_dns.cookies_dns,
+                                   headers=config_dns.headers_dns)
+
+            # Получаем страницу с 18 товарами, без блока цен и блока доступности
+            soup = BeautifulSoup(response.text, 'lxml')
+
+            # Добавляем в список имя товара
+            products_name.extend(soup.find_all('a', class_='catalog-product__name ui-link ui-link_black'))
+
+            # Находим все id товара
+            product_id1 = soup.find_all('span', class_='catalog-product__buy product-buy')
+            product_id2 = soup.find_all('div', class_='catalog-product ui-button-widget')
+
+            # Группируем id. Поскольку у 1 товара 2 id, нам нужно их сгруппировать, чтобы потом составить data-строку
+            products_ids = list(zip(product_id1, product_id2))
+
+            # Создаем списки price'ov и avails'ov. Поскольку на 192 строке мы добавляем все имена товаров
+            # на странице(18шт), а ниже мы проходим отдельно по каждому товару, то для дальшнейшего сопоставления
+            # имени, цены, доступности я создаю отдельные списки, которые после окончание цикла extend'ят списки
+            # prices, avails в общие списки products_price, products_available. Списки стираются при переходе на
+            # следующюю страницу.
+            prices = []
+            avails = []
+
+            # Проходим по каждому товару
+            for ind, pid in enumerate(products_ids):
+
+                # Создаем data-строку необходимую для post-запроса
+                data_price = f'data={{"type":"product-buy","containers":' \
+                             f'[{{"id":"{str(pid[0].get("id"))}","data":{{"id":"{str(pid[1].get("data-product"))}"}}}}]}}'
+                data_avail = f'data={{"type":"avails","containers":' \
+                             f'[{{"id":"{str(pid[0].get("id"))}","data":{{"id":"{str(pid[1].get("data-product"))}", "type":0,"useNotInStock":true}}}}]}}'
+
+                # Выполняем цикл который:
+                # 1) выполняет повторный запрос, при возникновении ошибки,
+                # 2) добавляет в списки prices, avails - цену и доступность, которую получаем из response
+                # post-запроса в JSON
+                while True:
+
+                    # Делаем запрос
+                    response_price = session.post(config_dns.url_price_dns,
+                                                  cookies=config_dns.cookies1_dns, headers=config_dns.headers1_dns,
+                                                  data=data_price,
+                                                  params=config_dns.params_dns)
+                    response_avail = session.post(config_dns.url_avail_dns,
+                                                  params=config_dns.params_dns,
+                                                  cookies=config_dns.cookies1_dns,
+                                                  headers=config_dns.headers1_dns, data=data_avail)
+
+                    # Выполняется условие: если reponse 200, то добавляем результат в список и выходим из цикла, иначе
+                    # если в response ошибка, то выполняем этоn же запрос повторно, пока не получим результат.
+                    if response_price.ok and response_avail.ok:
+
+                        # Явно достаем цену и доступность из JSON по ключам.
+                        current = response_price.json()['data']['states'][0]['data']['price']['current']
+                        avail = response_avail.json()['data']['states'][0]['data']['html']
+                        avails.append(avail)
+                        prices.append(current)
+                        break
+                    else:
+                        continue
+            products_price.extend(prices)
+            products_available.extend(avails)
+
+        # Создаем счетчик товаров
+        product_count = 0
+
+        # Группируем списки
+        products_list = list(zip(products_name, products_price, products_available))
+
+        # Создаем словарь в который добавляем полученные товары
+        for prod in products_list:
+            product_count += 1
+            # Avail необходимо распарсить, поскольку из JSON'a достается HTML-разметка, а не текст...
+            avail_soup = BeautifulSoup(prod[2], 'lxml')
+            avail_res = avail_soup.text
+            product_result_dns[product_count] = {
+                'product_name': prod[0].text,
+                'product_link': f"https://www.dns-shop.ru{prod[0].get('href')}",
+                'product_price': prod[1],
+                'product_available': True,  # пока что временно
+            }
+
+    # Делаем запись в БД
+    # return save_db(product_result_dns, 'DNS')
+
     return product_result_dns
 
 
-def get_pages() -> int:
-    '''
-    Функция предназначена для поиска количества страниц пагинации и загрузки печенек
-    '''
-
-    DRIVER.get('https://www.dns-shop.ru/catalog/17a89aab16404e77/videokarty/')
-    for cook in pickle.load(open('configs_shops/cookies_dns', 'rb')):
-        DRIVER.add_cookie(cook)
-    time.sleep(2)
-    DRIVER.refresh()
-
-    pages_count_link = DRIVER.find_element(By.CLASS_NAME, 'pagination-widget__page-link_last')
-    pages_count = int(re.findall(r'p=(\d\d)', str(pages_count_link.get_attribute('href')))[0])
-    return pages_count
-
-
 def main():
-    get_data_dns()
+    scraper_dns()
 
 
 if __name__ == '__main__':
