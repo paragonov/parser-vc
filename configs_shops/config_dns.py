6093eb1efff6a4fbc1b01884ba13924c1f22e621
vladimir <vladimirhoris@gmail.com>
2022-09-20 17:06:53 +0700

    убрал куки и хедеры

--- configs_shops/config_dns.py
+++ configs_shops/config_dns.py
@@ -1,2 +1,15 @@
-ua = 'user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-webdriver_disable = '--disable-blink-features=AutomationControlled'
+cookies_dns = ''
+headers_dns = ''
+
+cookies1_dns = ''
+
+headers1_dns = ''
+
+
+params_dns = ''
+
+url_dns = 'https://www.dns-shop.ru/catalog/17a89aab16404e77/videokarty/?stock=now&f[mv]=1dn4f0-udtje-13n3m1-udtf8-145iin-uiykt-v7hg2-zyyhm-1cwhi2-m2gz2-k1fh8'
+
+url_price_dns = "https://www.dns-shop.ru/ajax-state/product-buy/"
+
+url_avail_dns = 'https://www.dns-shop.ru/ajax-state/avails/'
